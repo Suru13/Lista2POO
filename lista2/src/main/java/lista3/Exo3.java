3e4b5c6b93a3cf6050c26fc5fd019ee8c5aba4c3
Suru13 <06538972780@cefet-rj.br>
2024-09-04 07:22:25 -0300

    Resolução exercício3 lista3

---
+++ lista2/src/main/java/lista3/Exo3.java
@@ -0,0 +1,44 @@
+package lista3;
+
+import java.util.Scanner;
+
+public class Exo3 {
+
+    public static void resolucao33(){
+
+        int num1,num2,menor,maior;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Informe o primeiro número: ");
+        num1 = scan.nextInt();
+        System.out.print("Informe o segundo número: ");
+        num2 = scan.nextInt();
+        if(num1<num2){
+            menor = num1;
+            maior = num2;
+        }
+        else{
+            menor = num2;
+            maior = num1;
+        }
+        System.out.println("\tOs números pares entre "+menor+" e "+maior+" são:");
+        for (int i = menor; i <= maior ; i++) {
+
+            if(ispar(i)){
+                System.out.print("\t"+i);
+            }
+        }
+    }
+
+    public static boolean ispar(int numero){
+
+        if(numero%2 ==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+}
--- lista2/src/main/java/lista3/Lista3Application.java
+++ lista2/src/main/java/lista3/Lista3Application.java
@@ -12,7 +12,7 @@ public class Lista3Application {
 		   SpringApplication.run(Lista3Application.class, args);
 
        //Exo1.resolucao31();
-       Exo2.resolucao32();
+       Exo3.resolucao33();
 
     }
 
