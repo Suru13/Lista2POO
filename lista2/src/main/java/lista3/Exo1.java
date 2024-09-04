9c05d45faf1c0a57b99fbfbd5b7dbf5cb223c523
Suru13 <06538972780@cefet-rj.br>
2024-09-04 06:24:49 -0300

    Resolução exercício1 lista3

---
+++ lista2/src/main/java/lista3/Exo1.java
@@ -0,0 +1,47 @@
+package lista3;
+
+
+import java.util.Scanner;
+
+public class Exo1 {
+
+
+    public static void resolucao31(){
+
+        int segundos;
+        boolean seg;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Informe o número de segundos entre 1 e 59 por favor:  ");
+        segundos = scan.nextInt();
+
+        seg = (segundos < 1 || segundos > 59);//segundos recebe true ou false
+
+        if(seg == true){
+
+             System.out.println("\tErro ao digitar um número fora do escopo do segundos!!!\n\tInforme por favor um número entre 1 e 59.");
+
+        }
+        else{
+
+            for (int i = 1; i <= segundos; i++) {
+
+                try {
+                    Thread.sleep(1000); // Aguarda 1 segundo
+                    
+                    System.out.println("\tSegundos: " + i);
+            
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                
+            }
+            System.out.println("\tAcabou o tempo!!!!");
+        }
+
+           
+    }
+
+}
+
---
+++ lista2/src/main/java/lista3/Lista3Application.java
@@ -0,0 +1,18 @@
+package lista3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Lista3Application {
+
+    public static void main(String[] args) {
+
+		   SpringApplication.run(Lista3Application.class, args);
+
+       Exo1.resolucao31();
+
+    }
+
+}
