4f940b713a986faffc2bc90da6b466ddc3dee125
Suru13 <06538972780@cefet-rj.br>
2024-09-07 19:34:29 -0300

    Resolução Exercício3 lista4

--- lista2/src/main/java/lista4/Lista4Application.java
+++ lista2/src/main/java/lista4/Lista4Application.java
@@ -14,7 +14,8 @@ public abstract class Lista4Application {
 		   SpringApplication.run(Lista4Application.class, args);
 
            //Exo1.resolucao41();
-           Exo2.resolucao42();
+           //Exo2.resolucao42();
+           Exo3.resolucao43();
 
     }
 
